93de8ed9a9f61f355ee49605ad187d5ddfb898ca
DanielGoulartt <nexuuz@hotmail.com>
2021-10-13 22:41:22 -0300

    First commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:14
+
+# Create app directory
+WORKDIR /app
+
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+COPY package*.json ./
+
+# Install app dependencies
+RUN npm install
+
+COPY . .
+
+RUN npm run build
+
+EXPOSE 3000
+CMD [ "npm", "run", "start:prod" ]
---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+  }),
+  TypeOrmModule.forRoot()
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
